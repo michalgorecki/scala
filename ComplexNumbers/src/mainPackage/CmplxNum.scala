d5f9965284bbd335a2259a26f0f9c752f6ac2526
Michal Gorecki <mih.gorecki@gmail.com>
2016-10-18 13:38:10 +0200

    First project upload

---
+++ ComplexNumbers/src/mainPackage/CmplxNum.scala
@@ -0,0 +1,10 @@
+
+
+package mainPackage
+
+class CmplxNum (real: Double, imaginary: Double){
+    def re = real
+    def im = imaginary
+    
+    override def toString() = "Real part = " + re + "; imaginary part = " + im
+}
---
+++ ComplexNumbers/src/mainPackage/Complex.scala
@@ -0,0 +1,12 @@
+package mainPackage
+
+
+object Complex {
+
+  def main(args: Array[String]) {
+    var myNumber: CmplxNum = new CmplxNum(1.4, 2.212387129847139871981)
+    println(myNumber.toString())
+    
+  }
+   
+}
